56e2f098d17d1eec24bfdcfff5943baf9dcbb9a7
prince prasad <pp1614582@gmail.com>
2025-04-23 01:27:56 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import Form from "./Form";
 
-const App = () => {
+const App = () => { 
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+      <Form />
+  </div>
+)
 }
+
 
 export default App
